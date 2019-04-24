7db7340aa09d1f2510dda5f1c1333f03d9957097
Nick Tardif <nicktardif@gmail.com>
2019-04-24 07:50:28 -0400

    Add in Flasgger documentation

--- sec_app/__init__.py
+++ sec_app/__init__.py
@@ -4,6 +4,9 @@ from sec_app.make_database import db
 from flask_migrate import Migrate
 from sec_app.utilities import CustomJSONEncoder
 from sec_app.views import LocationView
+from flasgger import Swagger
+
+swagger = Swagger(app, template=app.config['SWAGGER_TEMPLATE'])
 
 migrate = Migrate(app, db)
 app.json_encoder = CustomJSONEncoder
--- sec_app/views/location_view.py
+++ sec_app/views/location_view.py
@@ -6,6 +6,42 @@ from flask import jsonify
 class LocationView():
     @app.route('/api/v1/location/<int:location_id>')
     def get_location(location_id):
+        """Get a specified Location from the database
+        ---
+        definitions:
+            Location:
+                type: object
+                properties:
+                    id:
+                        type: integer
+                        example: 3
+                    name:
+                        type: string
+                        example: Dragonstone
+            ErrorResponse:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Description of error is here
+        tags:
+         - locations
+        parameters:
+            - in: query
+              description: Location ID to retrieve
+              name: location_id
+              required: true
+              type: integer
+        responses:
+            200:
+                description: Returns the location with the specified ID
+                schema:
+                    $ref: "#/definitions/Location"
+            404:
+                description: No location was found with the input ID
+                schema:
+                    $ref: "#/definitions/ErrorResponse"
+        """
         location = Location.query.get(location_id)
         if location:
             return jsonify(location), status.HTTP_200_OK
@@ -15,6 +51,20 @@ class LocationView():
 
     @app.route('/api/v1/locations')
     def get_all_locations():
+        """Get all locations from the database
+        ---
+        tags:
+         - locations
+        responses:
+            200:
+                description: Returns all the locations
+                schema:
+                    type: array
+                    items:
+                        $ref: "#/definitions/Location"
+            204:
+                description: No locations were in the database
+        """
         locations = Location.query.all()
         if locations:
             return jsonify(locations), status.HTTP_200_OK
