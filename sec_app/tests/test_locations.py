0dedbf28ff7e1b0ff6664e499d8af4a7f04f5b2d
Nick Tardif <nicktardif@gmail.com>
2019-04-25 08:17:32 -0400

    Addded Create and Delete Location API calls

--- sec_app/tests/test_locations.py
+++ sec_app/tests/test_locations.py
@@ -5,34 +5,38 @@ from sec_app.models import Location
 from sec_app.tests.sample_test_case import SampleTestCase
 import unittest
 
+def populate_database(db):
+    # Add some fake locations
+    locations = []
+    for name in ['Winterfell', 'The Wall', 'Hardhome']:
+        new_location = Location(name)
+        db.session.add(new_location)
+        locations.append(new_location)
+    db.session.commit()
+
 class GetLocationTest(SampleTestCase):
     def test_get_location(self):
-        # Fill the database with a fake location
-        new_location = Location('Winterfell')
-        db.session.add(new_location)
-        db.session.commit()
+        populate_database(db)
 
-        location_id = new_location.id
-        response = self.client.get('/api/v1/location/{}'.format(location_id))
+        location_id = 1
+        location = Location.query.get(location_id)
+
+        response = self.client.get('/api/v1/locations/{}'.format(location_id))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.json, new_location.toJSON())
+        self.assertEquals(response.json, location.toJSON())
 
     def test_get_location_invalid_id(self):
         location_id = 1
-        response = self.client.get('/api/v1/location/{}'.format(location_id))
+        response = self.client.get('/api/v1/locations/{}'.format(location_id))
         expected_response = {'error': 'Location with ID {} not found in the database'.format(location_id)}
         self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEquals(response.json, expected_response)
 
 class GetAllLocationsTest(SampleTestCase):
     def test_get_location(self):
-        # Add some fake locations
-        locations = []
-        for name in ['Winterfell', 'The Wall', 'Hardhome']:
-            new_location = Location(name)
-            db.session.add(new_location)
-            locations.append(new_location)
-        db.session.commit()
+        populate_database(db)
+
+        locations = Location.query.all()
 
         response = self.client.get('/api/v1/locations')
         self.assertEquals(response.status_code, status.HTTP_200_OK)
@@ -41,3 +45,49 @@ class GetAllLocationsTest(SampleTestCase):
     def test_get_location_no_data(self):
         response = self.client.get('/api/v1/locations')
         self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class CreateLocationTest(SampleTestCase):
+    def test_create_location(self):
+        data = {'name': 'Highgarden'}
+        response = self.client.post('/api/v1/locations', data=data)
+
+        expected_location_id = 1
+        expected_response = Location.query.get(expected_location_id)
+
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEquals(response.json, jsonify(expected_response).json)
+
+    def test_create_location_missing_name(self):
+        data = {'incorrect_field': 'bad'}
+        response = self.client.post('/api/v1/locations', data=data)
+
+        expected_response = {'error': 'Did not supply name in the data field'}
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.json, jsonify(expected_response).json)
+
+    def test_create_location_no_data_field(self):
+        response = self.client.post('/api/v1/locations')
+
+        expected_response = {'error': 'Request did not include a data form, try again'}
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.json, jsonify(expected_response).json)
+
+class DeleteLocationTest(SampleTestCase):
+    def test_delete_location(self):
+        populate_database(db)
+        location_id = 1
+        response = self.client.delete('/api/v1/locations/{}'.format(location_id))
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        # Check that the location is not in the database
+        location = Location.query.get(location_id)
+        self.assertEquals(location, None)
+
+    def test_delete_location_invalid_id(self):
+        location_id = 1
+        response = self.client.delete('/api/v1/locations/{}'.format(location_id))
+
+        expected_response = {'error': 'Location with the ID {} does not exist in the database'.format(location_id)}
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(response.json, jsonify(expected_response).json)
--- sec_app/views/location_view.py
+++ sec_app/views/location_view.py
@@ -1,10 +1,10 @@
 from sec_app import app, db
 from flask_api import status
 from sec_app.models import Location
-from flask import jsonify
+from flask import jsonify, request
 
 class LocationView():
-    @app.route('/api/v1/location/<int:location_id>')
+    @app.route('/api/v1/locations/<int:location_id>')
     def get_location(location_id):
         """Get a specified Location from the database
         ---
@@ -70,3 +70,69 @@ class LocationView():
             return jsonify(locations), status.HTTP_200_OK
         else:
             return '', status.HTTP_204_NO_CONTENT
+
+    @app.route('/api/v1/locations', methods=['POST'])
+    def create_location():
+        """Add a new Location to the database
+        ---
+        tags:
+         - locations
+        parameters:
+          - in: body
+            name: name
+            type: string
+            description: Name of the Location
+            required: true
+        responses:
+            201:
+                description: Location was created successfully
+                schema:
+                    $ref: "#/definitions/Location"
+            400:
+                description: Request was not formatted correctly
+                schema:
+                    $ref: "#/definitions/ErrorResponse"
+        """
+        if request.form:
+            if 'name' not in request.form:
+                message = 'Did not supply name in the data field'
+                return jsonify({'error': message}), status.HTTP_400_BAD_REQUEST
+            name = request.form.get('name')
+
+            new_location = Location(name)
+            db.session.add(new_location)
+            db.session.commit()
+
+            return jsonify(new_location), status.HTTP_201_CREATED
+        else:
+            message = 'Request did not include a data form, try again'
+            return jsonify({'error': message}), status.HTTP_400_BAD_REQUEST
+
+    @app.route('/api/v1/locations/<int:location_id>', methods=['DELETE'])
+    def delete_location(location_id):
+        """Delete a Location to the database
+        ---
+        tags:
+         - locations
+        parameters:
+          - in: query
+            name: location_id
+            type: integer
+            required: true
+        responses:
+            200:
+                description: Location deleted successfully
+            404:
+                description: Location with the specified ID does not exist in the database
+                schema:
+                    $ref: "#/definitions/ErrorResponse"
+        """
+        location = Location.query.get(location_id)
+
+        if location:
+            db.session.delete(location)
+            db.session.commit()
+            return '', status.HTTP_200_OK
+        else:
+            message = 'Location with the ID {} does not exist in the database'.format(location_id)
+            return jsonify({'error': message}), status.HTTP_404_NOT_FOUND
